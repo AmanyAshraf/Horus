93f7c39477fe3ad928ac589d200b2d868abe9976
elna977 <106814965+elna977@users.noreply.github.com>
2023-05-17 14:08:29 +0200

    design

--- app/src/main/java/com/example/horus/data/network/ApiService.kt
+++ app/src/main/java/com/example/horus/data/network/ApiService.kt
@@ -1,14 +1,18 @@
 package com.example.horus.data.network
 
 import com.example.horus.data.model.RegisterData
+import com.example.horus.data.model.DataProfile
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface ApiService {
 
     @POST("users/register")
     fun register(@Body body: RegisterData) : Call<Unit>
+    @POST("api/profiles")
+    fun createProfile(@Body body: DataProfile):Call<Unit>
 
 
 
--- app/src/main/java/com/example/horus/presentation/ui/activity/EditProfileActivity.kt
+++ app/src/main/java/com/example/horus/presentation/ui/activity/EditProfileActivity.kt
@@ -2,11 +2,31 @@ package com.example.horus.presentation.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.horus.R
+import com.example.horus.data.model.DataProfile
+import com.example.horus.data.network.ApiService
+import com.example.horus.databinding.ActivityEditProfileBinding
+import com.example.horus.domain.repository.DataCreateProfile
+import com.example.horus.presentation.viewmodel.CreateProfileViewModel
+import com.example.horus.presentation.viewmodel.MainViewModelFactory
 
 class EditProfileActivity : AppCompatActivity() {
+    private lateinit var viewModel: CreateProfileViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit_profile)
+        val binding =DataBindingUtil.setContentView<ActivityEditProfileBinding>(
+            this,R.layout.activity_edit_profile
+        )
+        val dataCreateProfile =DataCreateProfile()
+        val viewModelFactory=MainViewModelFactory(dataCreateProfile)
+        viewModel =ViewModelProvider(this,viewModelFactory).get(CreateProfileViewModel::class.java)
+
+        binding.btnSave.setOnClickListener {
+            viewModel.createProfile(dateOfBirth = "", phone = 1, codeCountry = "", userId = 0)
+        }
+
     }
 }
